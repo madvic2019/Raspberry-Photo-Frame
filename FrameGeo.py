e0eba34554c764ce55fead6f6fe47712a38a70ea
Victor Manuel Diaz Diaz <victor_manuel.diaz_diaz@nokia.com>
2020-05-04 11:37:01 +0200

    Update FrameGeo.py

--- FrameGeo.py
+++ FrameGeo.py
@@ -559,6 +559,6 @@ if __name__ == '__main__':
     print(args.path,args.config,args.waittime,"Shuffle ",args.shuffle)
     signal.signal(signal.SIGUSR2,handler2)
     signal.signal(signal.SIGUSR1, handler1)
-    main(startdir=args.path,config_file=args.config,interval=args.waittime,shuffle=args.shuffle,geoname=geouser)
+    main(startdir=args.path,config_file=args.config,interval=args.waittime,shuffle=args.shuffle,geonamesuser=geouser)
 
 
