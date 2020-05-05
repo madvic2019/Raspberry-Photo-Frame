2d250e81bc93ffd418f98624dbb7e2c5a1fefa35
Victor Manuel Diaz Diaz <victor_manuel.diaz_diaz@nokia.com>
2020-05-05 13:55:28 +0200

    Update FrameGeo.py

--- FrameGeo.py
+++ FrameGeo.py
@@ -413,6 +413,7 @@ def main(
             except:
               next_pic_num += 1
               continue
+            print("Have to reset timer!)  
             nexttm = time.time()+interval #reset timer to cope with texture delays
             
           if sbg is None: # first time through
