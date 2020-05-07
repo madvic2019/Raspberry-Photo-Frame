720e6a4dfb8ac064653db85d9aa86d1ee8789a5e
madvic2019 <53259325+madvic2019@users.noreply.github.com>
2020-05-07 17:23:16 +0200

    Update FrameGeo.py

--- FrameGeo.py
+++ FrameGeo.py
@@ -22,8 +22,19 @@ Support of geo tagging in EXIF to show location of photo in slide show (using Ge
 Persistent images list: enables restart with same images list and resume from last shown image.
 Optimized file list creation to enable (very) large images catalog
 
-This program is provided WITH ABSOLUTELY NO WARRANTY
 
+Copyright (c) Victor Diaz  2020
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 import os
 import time 
@@ -132,7 +143,7 @@ def get_coordinates(geotags):
       return None
 
 def get_geo_name(exif) : #Obtain geographic names from service provider
-  geocoder=geoloc.reverse(get_coordinates(get_geotagging(exif)),10,lang='es')
+  geocoder=geoloc.reverse(get_coordinates(get_geotagging(exif)),10,lang='es') #use your country code for language selection
   return geocoder
 
 def get_orientation(fname) : #extract orientation and capture date from EXIF data
