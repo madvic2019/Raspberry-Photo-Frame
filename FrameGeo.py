e430b6c7c54ef390921aede2af44891900cb028a
Victor Manuel Diaz Diaz <victor_manuel.diaz_diaz@nokia.com>
2020-05-08 11:42:26 +0200

    Update FrameGeo.py

--- FrameGeo.py
+++ FrameGeo.py
@@ -573,6 +573,7 @@ if __name__ == '__main__':
         )
     parser.add_argument(
         '--debug',
+        dest='debugprint',
         default=False,
         help='Print Debug Information'
         )
