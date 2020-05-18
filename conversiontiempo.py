9ff07160c8a5a60f36f4eb15ce71451308263114
Victor Manuel Diaz Diaz <victor_manuel.diaz_diaz@nokia.com>
2020-05-18 13:04:38 +0200

    Create conversiontiempo.py

---
+++ conversiontiempo.py
@@ -0,0 +1,33 @@
+#--coding: utf-8 --
+#!/usr/bin/python
+
+import argparse
+import time 
+import json
+import FrameConfig as config
+
+
+  
+parser = argparse.ArgumentParser(
+        description='Converts to readablie time information in Picture Frame config file '
+        )
+        
+parser.add_argument(
+        'path',
+        metavar='ImagePath',
+        type=str,
+        default=config.DEFAULT_CONFIG_FILE,
+        nargs="?",
+        help='Configuration file'
+        )
+args=parser.parse_args()
+
+
+try:
+  with open(args.path,'r') as f:
+    data=json.load(f)
+    print("Last File Change: ",time.localtime(data[2]))
+    print("Next Directory check: ",time.localtime(data[3]))
+
+except Exception as e:
+  print(e)
