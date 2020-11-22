ae9e84d1f338a0689ff30e4b51d104d58ac3ff68
madvic2019 <53259325+madvic2019@users.noreply.github.com>
2020-11-22 20:29:34 +0100

    Update check_button.py

--- check_button.py
+++ check_button.py
@@ -4,13 +4,15 @@
 from gpiozero import Button
 from time import sleep
 
-button = Button(2)
+buttons[] = {Button(9),Button(8),Button(4),Button(5),Button(6),Button(7)}
+
 
 while True:
-    if button.is_pressed:
-        print("Pressed")
-    else:
-        print("Released")
-    sleep(1)
+    for button in buttons :
+        if button.is_pressed:
+            print("Pressed")
+        else:
+            print("Released")
+        sleep(1)
 	
 	
