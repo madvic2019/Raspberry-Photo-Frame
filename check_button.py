6a00f333767134d998802546bb8cce2d4d6f315b
Victor Manuel Diaz Diaz <victor_manuel.diaz_diaz@nokia.com>
2020-11-23 16:57:49 +0100

    FromNokia

--- check_button.py
+++ check_button.py
@@ -1,18 +1,23 @@
 #--coding: utf-8 --
 #!/usr/bin/python3
 
-from gpiozero import Button
+from gpizero import ButtonBoard
 from time import sleep
 
-buttons = {Button(9),Button(8),Button(4),Button(5),Button(6),Button(7)}
+buttons = ButtonBoard(uno=9,dos=8,tres=4,cuatro=5,cinco=6,seis=7) 
 
-
-while True:
-    for button in buttons :
-        if button.is_pressed:
-            print("Pressed")
-        else:
-            print("Released")
-        sleep(1)
-	
-	
+while True :
+  
+  if buttons.uno.is_active :
+    print("Boton uno !!")
+  if buttons.dos.is_active :
+    print("Boton dos !!")
+  if button.tres.is_active :
+    print("Boton tres !!")
+  if buttons.cuatro.is_active :
+    print("Boton cuatro !!")
+  if buttons.cinco.is_active :
+    print("Boton cinco !!")
+  if button.seis.is_active :
+    print("Boton seis !!")
+    
