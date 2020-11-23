dada0d14d9fe585d46a5730e83bdcc61eca0b418
madvic2019 <53259325+madvic2019@users.noreply.github.com>
2020-11-23 19:53:23 +0100

    Update FrameGeo.py

--- FrameGeo.py
+++ FrameGeo.py
@@ -272,7 +272,6 @@ def timetostring(dot,ticks):
   return hour+separator+minutes
 
 def handle_button() :
-  button_pressed = None
   if buttons.atras.value == 1 :
     button_pressed = 1
   elif buttons.pause.value == 1 :
