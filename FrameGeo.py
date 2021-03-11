c7598d81f505b06aaf4548e36f02c8b982bad0a4
madvic2019 <53259325+madvic2019@users.noreply.github.com>
2021-03-11 19:11:21 +0100

    Update FrameGeo.py

--- FrameGeo.py
+++ FrameGeo.py
@@ -671,7 +671,7 @@ def main(
           next_pic_num -= 2
           if next_pic_num < -1:
             next_pic_num = -1
-          nexttm = 0 #force reload
+          #nexttm = 0 #force reload
           back_button.estado = 0
         
 
@@ -691,7 +691,7 @@ def main(
                 with open(iFiles[pic_num],'wb') as tmp_file: # Write the file with new exif orientation
                   tmp_file.write(tmp_im.get_file())
                 next_pic_num -=1 # force reload on screen
-                nexttm = 0
+                #nexttm = 0
 
         # All config.BUTTONS go to idle after processing them, regardless of state
        
