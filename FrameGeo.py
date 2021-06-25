21b29fecd74d9f3ca090505e056077442c2b5d44
madvic2019 <53259325+madvic2019@users.noreply.github.com>
2021-06-25 13:39:18 +0200

    Update FrameGeo.py

--- FrameGeo.py
+++ FrameGeo.py
@@ -674,16 +674,19 @@ def main(
             rotate_button.estado = 0
             nexttm = delta
             im.close() #close file on disk
-            with open(iFiles[pic_num],'rb') as tmp_file: #open file again to be used in exif context
-              tmp_im = exif.Image(tmp_file)
-              tmp_file.close() 
-              if (tmp_im.has_exif) : # If it has exif data, rotate it if it does not, do nothing
-                save_file(iFiles[pic_num]) # Copy file to Backup folder
-                tmp_im.orientation = rotate90CW(tmp_im.orientation) # changes EXIF data orientation parameter              
-                with open(iFiles[pic_num],'wb') as tmp_file: # Write the file with new exif orientation
-                  tmp_file.write(tmp_im.get_file())
-                next_pic_num -=1 # force reload on screen
-        
+            try:
+                with open(iFiles[pic_num],'rb') as tmp_file: #open file again to be used in exif context
+                  tmp_im = exif.Image(tmp_file)
+                  tmp_file.close() 
+                  if (tmp_im.has_exif) : # If it has exif data, rotate it if it does not, do nothing
+                    save_file(iFiles[pic_num]) # Copy file to Backup folder
+                    tmp_im.orientation = rotate90CW(tmp_im.orientation) # changes EXIF data orientation parameter              
+                    with open(iFiles[pic_num],'wb') as tmp_file: # Write the file with new exif orientation
+                      tmp_file.write(tmp_im.get_file())
+                    next_pic_num -=1 # force reload on screen
+            except:
+                print("Error when rotating photo")
+                
         if pause_button.estado == 1 or pause_button.estado == 2 : # button was pressed
           #nexttm = delta
           paused = not paused
