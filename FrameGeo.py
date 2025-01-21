fb00078ac4f5a49702d9942a0fac4974c7901dfb
madvic2019 <victormanuel.diazdiaz@gmail.com>
2025-01-21 14:09:20 +0100

    Updated Exif

--- FrameGeo.py
+++ FrameGeo.py
@@ -165,9 +165,9 @@ def get_geotagging(exif): # extract EXIF geographical information
     
 def get_decimal_from_dms(dms, ref): 
 
-    degrees = dms[0][0] / dms[0][1]
-    minutes = dms[1][0] / dms[1][1] / 60.0
-    seconds = dms[2][0] / dms[2][1] / 3600.0
+    degrees = dms[0]
+    minutes = dms[1] / 60.0
+    seconds = dms[2] / 3600.0
 
     if ref in ['S', 'W']:
         degrees = -degrees
