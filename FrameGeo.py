63bc246ee06b567b9a81f8b8abd3b5ea3153fa21
madvic2019 <53259325+madvic2019@users.noreply.github.com>
2025-05-23 13:25:51 +0200

    º1

--- FrameGeo.py
+++ FrameGeo.py
@@ -156,7 +156,7 @@ def launchTiempo() :
   subprocess.Popen(['firefox','--kiosk','https://www.aemet.es/es/eltiempo/prediccion/municipios/alcala-de-henares-id28005'])
   time.sleep(10)
   subprocess.Popen(['xdotool','key','Down','Down','Down','Down'])
-  time.sleep(300)
+  time.sleep(30)
   os.killpg(os.getpgid(), signal.SIGTERM) # kill all processes in the group
 
   
