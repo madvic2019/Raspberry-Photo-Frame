f63b11f998ee1d5d3c76aa9f7097569aa83d2d3f
Madvic2019 <victormanuel.diazdiaz@gmail.com>
2025-06-28 01:04:54 +0200

    Rotating log file

--- FrameGeo.py
+++ FrameGeo.py
@@ -48,6 +48,8 @@ import math
 import subprocess
 import signal
 import logging 
+from logging.handlers import RotatingFileHandler
+
 
 
 
@@ -405,6 +407,8 @@ def main(
   
     global backup_dir,paused,geoloc,last_file_change,kb_up,FIT,BLUR_EDGES,screen
   # Set up logging  
+    rotating_handler = RotatingFileHandler(logfile,maxBytes=1024*1024*10,backupCount=3)
+    
     if debug:
         loglevel=logging.DEBUG
     else:
@@ -414,7 +418,8 @@ def main(
     filename=logfile,  # Save logs to a file
     level=loglevel,  # Set the logging level
     format='%(pathname)s: %(asctime)s - %(levelname)s:%(message)s',  # Customize log format
-            datefmt='%m/%d/%Y %I:%M:%S %p')
+            datefmt='%m/%d/%Y %I:%M:%S %p',
+    handlers=[rotating_handler, logging.StreamHandler()]  # Add both file and console handlers)
     logging.info("Starting FrameGeo with parameters: startdir=%s,config_file=%s,interval=%d,shuffle=%s,geonamesuser=%s,check_dirs=%d,weathertime=%d,logfile=%s",
                 startdir,
                 config_file,
