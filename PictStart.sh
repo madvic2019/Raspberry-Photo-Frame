6bd3ca0536af1e29857a9f550f35489da90c3d9e
madvic2019 <victormanuel.diazdiaz@gmail.com>
2025-07-02 11:42:20 +0200

    m

--- PictStart.sh
+++ PictStart.sh
@@ -4,7 +4,7 @@ export DISPLAY=:0
 pgrep 'FrameGeo'
 if [ $? != 0 ]
 then
-~/mypy/bin/python3 $HOME/Raspberry-Photo-Frame/FrameGeo.py --config-file $HOME/.photo-frame --geouser madvic --dir-check 360000 --waittime 20 --weather-time 300 --logfile /mnt/frododata/temp/Framegeo.log /mnt/frodomedia/photo/Fotos
+python $HOME/Raspberry-Photo-Frame/FrameGeo.py --config-file $HOME/.photo-frame --geouser madvic --dir-check 360000 --waittime 20 --weather-time 300 --logfile /mnt/frododata/temp/Framegeo.log /mnt/frodomedia/photo/Fotos
 
 fi
 
